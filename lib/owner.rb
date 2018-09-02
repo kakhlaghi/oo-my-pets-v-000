47f5bd968207d2a27c2b18cda9bb683528f0862b
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-02 23:30:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,5 +66,7 @@ class Owner
     fish_count = @pets[:fishes].count
     dogs_count = @pets[:dogs].count
     cats_count = @pets[:cats].count
+    
+    return "I have #{fish_count} fish, #{dog_count} dog(s), and #{cats_count} cat(s)."
   end
 end
