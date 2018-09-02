ffaaa0d72c6781263d7e5f17ab614000fd441329
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-02 20:26:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_reader :owner
+  attr_accessor :pets
+  @@all = []
+  def initialize(owner, pets = {fishes: [], cats: [], dogs: []})
+    @owner = "human"
+    @pets = pets
+    
+  end
+  
+  def self.all
+    @@all << self
+  end  
 end
